8932e62521dd5bc597b91c935153d7af523759a8
rhradec <me@hradec.com>
2025-03-26 17:05:06 -0700

    change for multiple-photos-scan

--- src/depth_pro/depth_pro.py
+++ src/depth_pro/depth_pro.py
@@ -285,7 +285,8 @@ class DepthPro(nn.Module):
             f_px = 0.5 * W / torch.tan(0.5 * torch.deg2rad(fov_deg.to(torch.float)))
         
         inverse_depth = canonical_inverse_depth * (W / f_px)
-        f_px = f_px.squeeze()
+        if not isinstance(f_px, float):
+            f_px = f_px.squeeze()
 
         if resize:
             inverse_depth = nn.functional.interpolate(
